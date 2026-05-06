1ea9b818b165990372c1476c75fbd4118f718834
Levent Gulderen <mrgulderen2@gmail.com>
2026-05-06 23:16:05 +0200

    change the file paths

--- scripts/load_bronze.sql
+++ scripts/load_bronze.sql
@@ -18,7 +18,7 @@ Warning:
 TRUNCATE TABLE bronze.crm_cust_info;
 
 BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\Mrgul\Desktop\Ondia\server_sql\Baraa_Sql\data\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+FROM 'C:\Users\Mrgul\Desktop\Ondia\sql_dtwh_project\datasets\source_crm\cust_info.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -29,7 +29,7 @@ GO
 TRUNCATE TABLE bronze.crm_prd_info;
 
 BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\Mrgul\Desktop\Ondia\server_sql\Baraa_Sql\data\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+FROM 'C:\Users\Mrgul\Desktop\Ondia\sql_dtwh_project\datasets\source_crm\prd_info.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -40,7 +40,7 @@ GO
 TRUNCATE TABLE bronze.crm_sales_details;
 
 BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\Mrgul\Desktop\Ondia\server_sql\Baraa_Sql\data\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+FROM 'C:\Users\Mrgul\Desktop\Ondia\sql_dtwh_project\datasets\source_crm\sales_details.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -51,7 +51,7 @@ GO
 TRUNCATE TABLE bronze.erp_cust_az12;
 
 BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\Mrgul\Desktop\Ondia\server_sql\Baraa_Sql\data\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+FROM 'C:\Users\Mrgul\Desktop\Ondia\sql_dtwh_project\datasets\source_erp\CUST_AZ12.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -62,7 +62,7 @@ GO
 TRUNCATE TABLE bronze.erp_loc_a101;
 
 BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\Mrgul\Desktop\Ondia\server_sql\Baraa_Sql\data\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+FROM 'C:\Users\Mrgul\Desktop\Ondia\sql_dtwh_project\datasets\source_erp\LOC_A101.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
@@ -73,7 +73,7 @@ GO
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
 BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\Mrgul\Desktop\Ondia\server_sql\Baraa_Sql\data\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+FROM 'C:\Users\Mrgul\Desktop\Ondia\sql_dtwh_project\datasets\source_erp\PX_CAT_G1V2.csv'
 WITH (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
